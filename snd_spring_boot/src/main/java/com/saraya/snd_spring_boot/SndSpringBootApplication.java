5c1b7bc6ee0ccff0c3090c50efe7b6cbbe28e34f
TidianiAmada <tidiani.amada@gmail.com>
2021-09-09 21:45:08 +0200

    crud functionality except add

---
+++ snd_spring_boot/src/main/java/com/saraya/snd_spring_boot/SndSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.saraya.snd_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SndSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SndSpringBootApplication.class, args);
+	}
+
+}
